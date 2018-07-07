b3ba3935c5478ee115d287c13d7eecc706dc87d0
nathalie <nathalie.scruz@gmail.com>
2018-07-07 05:13:55 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,21 +15,35 @@ class Transfer
   end
 
   def execute_transaction
+<<<<<<< HEAD
     if self.valid? && @sender.balance > @amount && @status== "pending"
       @sender.balance = @sender.balance - @amount
       #binding.pry
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
     else
+=======
+    if self.valid? & self.status == "pending"
+      @sender -= @amount
+      @receiver += @amount
+      @status = "complete"
+    else #self.valid? == false
+>>>>>>> f7440fe74a0f0f6a1d52f79c9bde688ba58cf98d
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
 
   def reverse_transfer
+<<<<<<< HEAD
     if @status == "complete"
     @sender.balance += @amount
     @receiver.balance -= @amount
+=======
+    if @status = "complete"
+    @sender += @amount
+    @receiver -= @amount
+>>>>>>> f7440fe74a0f0f6a1d52f79c9bde688ba58cf98d
     @status = "reversed"
   end
 
